af8afac755b3f1348da7da0374a3b620e43cf803
Shete_Vinayak <shetevinay8314@gmail.com>
2023-05-30 23:21:54 +0530

    project upload

---
+++ src/main/java/com/mycompany/login_registration/Login_registration.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.login_registration;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Login_registration {
+
+    public static void main(String[] args) {
+        Frontpage fp=new Frontpage();
+        fp.setVisible(true);
+//       Register obj = new Register();
+//       obj.setVisible(true);
+//       Login obj2=new Login();
+//       obj2.setVisible(true);
+        
+    }
+}
---
+++ src/main/java/com/mycompany/oip/OIP.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.oip;
+
+import com.mycompany.login_registration.AppReg;
+import com.mycompany.login_registration.Frontpage;
+
+/**
+ *
+ * @author DELL
+ */
+public class OIP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+            Frontpage ob = new Frontpage();
+            ob.setVisible(true);
+    }
+    
+}
